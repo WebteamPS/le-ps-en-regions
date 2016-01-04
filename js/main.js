2e96450472429d375ef30b37d73fe29fe0a4d6f6
WebteamPS <webmasterparti-socialiste.fr>
2016-01-04 16:23:38 +0100

    fixe bug on IE

--- js/main.js
+++ js/main.js
@@ -26,3 +26,9 @@ $('#graphBourgogne').circliful();
 $('#graphAquitaine').circliful();
 $('#graphCentre').circliful();
 $('#graphLanguedoc').circliful();
+
+function supportsSvg() {
+  var div = document.createElement('div');
+  div.innerHTML = '<svg/>';
+  return (div.firstChild && div.firstChild.namespaceURI) == 'http://www.w3.org/2000/svg';
+};
