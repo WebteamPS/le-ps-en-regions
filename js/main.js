bd8cca8a0f407988550011c6abf32422e125cb3e
WebteamPS <webmasterparti-socialiste.fr>
2016-01-04 14:17:40 +0100

    add alls files sources

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.showSingle, .itemShow').on('click', function () {
+    $(this).attr('class', 'active').siblings().removeAttr('class', 'active');
+    $('.targetInfo').hide();
+    $('#info' + $(this).attr('target')).show(function(){
+    	$(this).animate({opacity: 1});
+    });
+});
+$('.showSingle, .itemShow').first().click();
+
+//http://jsfiddle.net/x7xqb/1/
+//https://martinwolf.org/2014/12/11/adding-and-removing-classes-from-svg-elements-with-jquery/
+
+// GO TO
+$('a[href^="#"]').click(function(){
+  var the_id = $(this).attr("href");
+
+  $('html, body').animate({
+    scrollTop:$(the_id).offset().top
+  }, 'slow');
+  return false;
+});
+
+//https://github.com/pguso/jquery-plugin-circliful
+$('#graphBretagne').circliful();
+$('#graphBourgogne').circliful();
+$('#graphAquitaine').circliful();
+$('#graphCentre').circliful();
+$('#graphLanguedoc').circliful();
